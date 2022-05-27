cf3a45c033f2ecc38d51f469812586c0c661d2c7
Oscar Ayala <oscarfabianayalazapata@gmail.com>
2022-05-27 00:51:17 -0400

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
- 
+  templateUrl: './app.component.html'
 })
+
+
+
 export class AppComponent {
   
-palabra = 'palta';
+palabra = 'PALTA';
 palabraOculta = '';
+
+intentos = 0;
+
+gano = true;
+perdio = false;
 
   letras = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J',
             'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
@@ -17,6 +24,55 @@ palabraOculta = '';
 
   constructor(){
     this.palabraOculta = '- '.repeat(this.palabra.length);
+  }
+
+  comprobar( letra: string ){
+
+    this.existeLetra(letra);
+    
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for (let i = 0; i < this.palabraOculta.length; i++) {
+      
+      if (this.palabra[i] === letra){
+
+        palabraOcultaArr[i] = letra;
+      }
+      
+    }
+
+    this.palabraOculta = palabraOcultaArr.join(' ');
+
+    this.verificaGane();
+  }
+
+  verificaGane(){
+
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+    
+    if (palabraEvaluar === this.palabra) {
+      this.gano = true;
+      console.log('Usuario GANO');
+    }
+
+    if (this.intentos >= 9 ) {
+      this.perdio = true;
+      console.log('Usuario perdio');
+      
+    }
+
+  }
+
+  existeLetra(letra: string){
+
+    if(this.palabra.indexOf(letra) >=0 ){
+     // console.log('Letra existe '+ letra);
+    }else{
+      //console.log('Letra no existe '+ letra);
+      this.intentos++;
+    }
+
   }
 
 
