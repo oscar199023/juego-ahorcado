04307ed23f4b4a0bab22dd275e7322add6dee791
Oscar Ayala <oscarfabianayalazapata@gmail.com>
2022-05-26 22:25:03 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ 
 })
 export class AppComponent {
-  title = 'ahorcado';
+  
+palabra = 'palta';
+palabraOculta = '';
+
+  letras = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+  constructor(){
+    this.palabraOculta = '- '.repeat(this.palabra.length);
+  }
+
+
 }
